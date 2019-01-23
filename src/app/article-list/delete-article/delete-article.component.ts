1001ca15620c487cce7ce0b4bcbebfd4e4d7e1d4
sundarsowmiya <sundar.n1987@gmail.com>
2019-01-24 02:59:39 +0530

    Update delete-article.component.ts

--- src/app/article-list/delete-article/delete-article.component.ts
+++ src/app/article-list/delete-article/delete-article.component.ts
@@ -1,41 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import {Location} from '@angular/common';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {
+    Component, OnInit
+}
+from '@angular/core';
+import {
+    Location
+}
+from '@angular/common';
+import {
+    FormBuilder, FormGroup
+}
+from '@angular/forms';
 
-import {ActivatedRoute, Router} from '@angular/router';
+import {
+    ActivatedRoute, Router
+}
+from '@angular/router';
 
-import { ArticleService } from '../../services/article.service'
+import {
+    ArticleService
+}
+from '../../services/article.service'
 
 @Component({
-  selector: 'app-delete-article',
-  templateUrl: './delete-article.component.html',
-  styleUrls: ['./delete-article.component.css']
+    selector: 'app-delete-article',
+    templateUrl: './delete-article.component.html',
+    styleUrls: ['./delete-article.component.css']
 })
 export class DeleteArticleComponent implements OnInit {
-  DeleteArticlForm;
+    DeleteArticlForm;
+
+    constructor(
+        private _location: Location,
+        private route: ActivatedRoute,
+        private deleteService: ArticleService, f: FormBuilder) {
+
+        this.DeleteArticlForm = f.group({});
+    }
+
+    ngOnInit() {}
+    backClicked() {
+        this._location.back();
+    }
   
-  constructor(
-    private _location: Location, 
-    private route: ActivatedRoute, 
-    private deleteService:ArticleService, f:FormBuilder) {
-    
-    this.DeleteArticlForm=f.group({
-    });
-  }
-
-  ngOnInit() {
-  }
-   backClicked() {
-    this._location.back();
-  }
-  deleteArticle(){
-    this.deleteService.deletearticle(this.route.snapshot.params.id).subscribe(
-      (response)=> {window.location.href = "/article-list";
-        //this.sharedService.changeData(true);
-        this.backClicked();
-      },
-      (error) => console.log(error)
-    )    
-  }
-
+    deleteArticle() {
+        this.deleteService.deletearticle(this.route.snapshot.params.id).subscribe(
+            (response) => {window.location.href = "/article-list";
+               
+            }, (error) => console.log(error)
+        )
+    }
 }
