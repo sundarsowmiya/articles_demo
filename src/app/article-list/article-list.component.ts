569d750f64c9d15fa80129d5a96cbf62df6309cd
SUNDAR <sundar.n1987@gmail.com>
2019-01-25 22:43:17 +0530

    Update article-list.component.ts

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -27,92 +27,97 @@ import {
 
 from 'rxjs';
 import * as config from '../../globalConfig';
-
+//import {TitleService} from 'title.service';
 import {
     SharedService
 }
 
 from '../services/shared.service';
-@Component( {
-    selector: 'app-article-list', templateUrl: './article-list.component.html', styleUrls: ['./article-list.component.css'], encapsulation: ViewEncapsulation.None
-}
+@Component({
+        selector: 'app-article-list',
+        templateUrl: './article-list.component.html',
+        styleUrls: ['./article-list.component.css'],
+        encapsulation: ViewEncapsulation.None
+    }
 
 ) export class ArticleListComponent implements OnInit {
-    dtOptions:DataTables.Settings= {}
-    ;
-    constructor( private route: ActivatedRoute, //private titleService:TitleService,
-    private renderer: Renderer, private router: Router, private sharedService: SharedService) {}
+    dtOptions: DataTables.Settings = {};
+    constructor(private route: ActivatedRoute, //private titleService:TitleService,
+        private renderer: Renderer, private router: Router, private sharedService: SharedService) {}
     ngOnInit() {
         this.renderDatatable();
-        this.sharedService.currentData.subscribe((data)=> {
-            if(data) {
+        this.sharedService.currentData.subscribe((data) => {
+            if (data) {
                 console.log(data, "Service change");
             }
-        }
-        )
+        })
     }
     ngAfterViewInit(): void {
-        this.renderer.listenGlobal('document', 'click', (event)=> {
+        this.renderer.listenGlobal('document', 'click', (event) => {
             if (event.target.hasAttribute("article-edit-id")) {
-                this.router.navigate(["article-list/edit-article/"+ event.target.getAttribute("article-edit-id")], {
+                this.router.navigate(["article-list/edit-article/" + event.target.getAttribute("article-edit-id")], {
                     queryParams: {
                         app: "article"
                     }
-                }
-                );
+                });
             }
-            if(event.target.hasAttribute("article-delete-id")) {
-                this.router.navigate(["article-list/delete-article/"+ event.target.getAttribute("article-delete-id")], {
+            if (event.target.hasAttribute("article-delete-id")) {
+                this.router.navigate(["article-list/delete-article/" + event.target.getAttribute("article-delete-id")], {
                     queryParams: {
                         app: "article"
                     }
-                }
-                );
+                });
             }
-        }
-        );
+        });
     }
     renderDatatable() {
-        this.dtOptions= {
+        this.dtOptions = {
             ajax: {
-                "url": config.gServiceUrl+"articles/read_articles.php",
-                "dataSrc":function ( json) {
+                "url": config.gServiceUrl + "articles/read_articles.php",
+                "dataSrc": function(json) {
                     console.log(json);
-                    let myJson= {data: []};
-                    myJson.data.push(json.records) return myJson.data[0];
+                    let myJson = {
+                        data: []
+                    };
+                    myJson.data.push(json.records);
+                    return myJson.data[0];
                 }
-            }
-            ,
-            columns: [ {
-                title: 'Title', data: 'article_title'
-            }
-            ,
-            {
-                title: 'Description', data: 'article_descrip'
-            }
-            ,
-            {
-                title: 'Author Name', data: 'article_author_name'
-            }
-            ,
-            {
-                title: 'Tags', data: 'article_tag'
-            }
-            ,
-            {
-                title: 'Publish Date', data: 'article_publish_date'
-            }
-            ,
-            {
-                title: 'Option',
-                data: null,
-                className: "center",
-                render: function (data, type, row) {
-                    return `<a article-edit-id="`+data.article_id+`" data-toggle="modal" class="btn default btn-xs purple"><i class="mdi mdi-square-edit-outline"></i> Edit </a> <a article-delete-id="`+data.article_id+`" data-toggle="modal" class="btn default btn-xs black"><i class="mdi mdi-delete"></i> Delete</a> `;
+            },
+            columns: [{
+                    title: 'Title',
+                    data: 'article_title'
+                },
+                {
+                    title: 'Description',
+                    data: null,
+                    render: function(data, type, row) {
+                    return `<span class="md-text-adj" >` + data.article_descrip +`</span>`;
+                    }
+                },
+                {
+                    title: 'Author Name',
+                    data: 'article_author_name'
+                },
+                {
+                    title: 'Tags',
+                    data: null,
+                    render: function(data, type, row) {
+                    return `<span class="md-text-adj" >` + data.article_tag +`</span>`;
+                    }
+                },
+                {
+                    title: 'Publish Date',
+                    data: 'article_publish_date'
+                },
+                {
+                    title: 'Option',
+                    data: null,
+                    className: "center",
+                    render: function(data, type, row) {
+                        return `<a article-edit-id="` + data.article_id + `" data-toggle="modal" class="btn default btn-xs purple"><i class="mdi mdi-square-edit-outline"></i> Edit </a> <a article-delete-id="` + data.article_id + `" data-toggle="modal" class="btn default btn-xs black"><i class="mdi mdi-delete"></i> Delete</a> `;
+                    }
                 }
-            }
             ]
-        }
-        ;
+        };
     }
 }
